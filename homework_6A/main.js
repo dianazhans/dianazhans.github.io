0531a226b3f7849b48fe86867f1d0c91a3e09fa0
Diana Zhan <dianazhanux@gmail.com>
2019-10-20 19:46:13 -0400

    added product details images and dynamic display on details page

--- homework_6A/main.js
+++ homework_6A/main.js
@@ -1,16 +1,21 @@
 //stores the title of the item//
-document.getElementById("rollTitle").innerHTML = localStorage.getItem("rollName");
+document.getElementById("rollName").innerHTML = localStorage.getItem("rollName");
 
 //stores the price of the item//
 document.getElementById("rollPrice").innerHTML = localStorage.getItem("rollPrice");
+
+//stores the product-details image of the item//
+document.getElementById("rollImage").src = localStorage.getItem("rollImage");
 
 //populates the name and price of the item that was clicked on//
 function getTitlePriceFromClick(e) {
   //sets a variable for the title and price of item//
   var name = e.target.dataset.name;
   var price = e.target.dataset.price;
+  var rollImage = e.target.dataset.image;
 
   //sets the title and price of the target object into the products-details page//
-  localStorage.setItem("rollTitle", name);
+  localStorage.setItem("rollName", name);
   localStorage.setItem("rollPrice", price);
+  localStorage.setItem("rollImage", "images/" + rollImage);
 }
