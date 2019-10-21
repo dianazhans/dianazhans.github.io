16cc872e7fb700a17837ede7f89e61c9e4166def
Diana Zhan <dianazhanux@gmail.com>
2019-10-21 11:23:27 -0400

    included add to cart functionality

--- homework_6A/main.js
+++ homework_6A/main.js
@@ -1,13 +1,13 @@
-//stores the title of the item//
+//loads the stored title of the item//
 document.getElementById("rollName").innerHTML = localStorage.getItem("rollName");
 
-//stores the price of the item//
+//loads the stored price of the item//
 document.getElementById("rollPrice").innerHTML = localStorage.getItem("rollPrice");
 
-//stores the product-details image of the item//
+//loads the stored product-details image of the item//
 document.getElementById("rollImage").src = localStorage.getItem("rollImage");
 
-//populates the name and price of the item that was clicked on//
+//populates the name, price and image of the item that was clicked on//
 function getTitlePriceFromClick(e) {
   //sets a variable for the title and price of item//
   var name = e.target.dataset.name;
@@ -18,4 +18,17 @@ function getTitlePriceFromClick(e) {
   localStorage.setItem("rollName", name);
   localStorage.setItem("rollPrice", price);
   localStorage.setItem("rollImage", "images/" + rollImage);
+}
+
+//sets initial item number of 0//
+var currentItems = 0;
+
+//displays and updates number of items in cart icon//
+function updateCartItemNumber() {
+  //display the cart item number//
+  document.getElementById("cart-item-number-display").style.display = "inline-block";
+  //adds 1 additional item upon click//
+  currentItems++;
+  //updates the cart item number//
+  document.getElementById("cart-item-number").innerHTML = currentItems;
 }
