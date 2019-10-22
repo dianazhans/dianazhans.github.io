014a6eefe7b73a74675e48fc9853fdc4571a9145
Diana Zhan <dianazhanux@gmail.com>
2019-10-21 22:20:16 -0400

    included cart icon that is persistent across pages

--- homework_6A/main.js
+++ homework_6A/main.js
@@ -1,3 +1,9 @@
+//if there are items in cart, then display it//
+if (localStorage.getItem("currentItems") > 0) {
+  document.getElementById("cart-item-number-display").style.display = "inline-block";
+  document.getElementById("cart-item-number").innerHTML = localStorage.getItem("currentItems");
+}
+
 //loads the stored title of the item//
 document.getElementById("rollName").innerHTML = localStorage.getItem("rollName");
 
@@ -8,27 +14,28 @@ document.getElementById("rollPrice").innerHTML = localStorage.getItem("rollPrice
 document.getElementById("rollImage").src = localStorage.getItem("rollImage");
 
 //populates the name, price and image of the item that was clicked on//
-function getTitlePriceFromClick(e) {
-  //sets a variable for the title and price of item//
+function getTitlePriceImageFromClick(e) {
+  //sets variables for the title, price and image of item//
   var name = e.target.dataset.name;
   var price = e.target.dataset.price;
   var rollImage = e.target.dataset.image;
 
-  //sets the title and price of the target object into the products-details page//
+  //sets the title, price and image of the target object into the products-details page//
   localStorage.setItem("rollName", name);
   localStorage.setItem("rollPrice", price);
   localStorage.setItem("rollImage", "images/" + rollImage);
 }
 
-//sets initial item number of 0//
-var currentItems = 0;
+//check if there are any items in cart, if not, then set currentItems to 0//
+if (localStorage.getItem("currentItems") == null) {
+  localStorage.setItem("currentItems", 0);
+}
 
-//displays and updates number of items in cart icon//
+//updates cart item number and display status upon click of add to cart button//
 function updateCartItemNumber() {
-  //display the cart item number//
-  document.getElementById("cart-item-number-display").style.display = "inline-block";
-  //adds 1 additional item upon click//
-  currentItems++;
-  //updates the cart item number//
-  document.getElementById("cart-item-number").innerHTML = currentItems;
+  var itemNumber = localStorage.getItem("currentItems"); //sets a variable to the currentItems//
+  itemNumber++; //adds 1 to the variable//
+  localStorage.setItem("currentItems", itemNumber); //sets the new number to currentItems//
+  document.getElementById("cart-item-number").innerHTML = localStorage.getItem("currentItems"); //updates the innerHTMl of the cart number//
+  document.getElementById("cart-item-number-display").style.display = "inline-block";//displays the cart number in header//
 }
