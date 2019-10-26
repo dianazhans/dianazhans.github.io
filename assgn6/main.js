40bfc29ac14abf663fbe9698f1eeb29ae605316a
Diana Zhan <dianazhanux@gmail.com>
2019-10-26 18:13:19 -0400

    added functionality to retrieve local storage items and load on shopping cart

--- assgn6/main.js
+++ assgn6/main.js
@@ -1,7 +1,12 @@
 //if there are items in cart, then display it//
-if (localStorage.getItem("currentItems") > 0) {
-  document.getElementById("cart-item-number-display").style.display = "inline-block";
-  document.getElementById("cart-item-number").innerHTML = localStorage.getItem("currentItems");
+function loadCartInHeader(){
+  if (localStorage.getItem("currentItems") > 0) {
+    document.getElementById("cart-item-number-display").style.display = "inline-block";
+    document.getElementById("cart-item-number").innerHTML = localStorage.getItem("currentItems");
+  }
+};
+
+function loadCartTitleItemNumber() {
   document.getElementById("cartTitleItemNumber").innerHTML = localStorage.getItem("currentItems");
 }
 
@@ -23,11 +28,15 @@ function getTitlePriceImageFromClick(e) {
   var name = e.target.dataset.name;
   var price = e.target.dataset.price;
   var rollImage = e.target.dataset.image;
+  //sets variable for image of roll to be used in shopping cart//
+  var cartRollImage = e.target.src;
 
   //sets the title, price and image of the target object into the products-details page//
   localStorage.setItem("rollName", name);
   localStorage.setItem("rollPrice", price);
   localStorage.setItem("rollImage", "images/" + rollImage);
+  //stores image of roll to be used in shopping cart//
+  localStorage.setItem("cartRollImage", cartRollImage);
 }
 
 //check if there are any items in cart, if not, then set currentItems to 0//
@@ -36,20 +45,30 @@ if (localStorage.getItem("currentItems") == null) {
 }
 
 //updates cart item number and display status upon click of add to cart button//
-function updateCartItemNumber() {
+function addToCart() {
+  //updates cart item number//
   var itemNumber = localStorage.getItem("currentItems"); //sets a variable to the currentItems//
   itemNumber++; //adds 1 to the variable//
   localStorage.setItem("currentItems", itemNumber); //sets the new number to currentItems//
   document.getElementById("cart-item-number").innerHTML = localStorage.getItem("currentItems"); //updates the innerHTMl of the cart number//
   document.getElementById("cart-item-number-display").style.display = "inline-block";//displays the cart number in header//
-  document.getElementById("cartTitleItemNumber").innerHTML = localStorage.getItem("currentItems");
+
+  //stores number of rolls and glaze//
+  var rollNumber = document.getElementById("select-number-of-rolls").value;
+  var rollGlaze = document.getElementById("select-glaze").value;
+  localStorage.setItem("rollNumber", rollNumber);
+  localStorage.setItem("rollGlaze", rollGlaze);
 }
 
 //ASSIGNMENT 6B//
 
-//get roll image for shopping cart//
-//get number of rolls//
-//get glaze//
+function loadCart() {
+  document.getElementById("cartRollImage").src = localStorage.getItem("cartRollImage");
+  document.getElementById("rollName").innerHTML = localStorage.getItem("rollName");
+  document.getElementById("rollPrice").innerHTML = localStorage.getItem("rollPrice");
+  document.getElementById("rollGlaze").innerHTML = localStorage.getItem("rollGlaze");
+  document.getElementById("select-number-of-rolls").value = localStorage.getItem("rollNumber");
+}
 
 //PROCESS//
 //create an empty array for populating products//
